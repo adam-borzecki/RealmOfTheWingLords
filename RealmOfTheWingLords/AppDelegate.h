25ccbb313ff06339eb2a51aa60c157ef5b436d47
adam-borzecki <adam.borzecki@gmail.com>
2013-08-21 09:30:43 -0400

    Initial Commit

---
+++ RealmOfTheWingLords/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RealmOfTheWingLords
+//
+//  Created by Adam Borzecki on 2013-08-21.
+//  Copyright (c) 2013 Adam Borzecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RealmOfTheWingLords/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RealmOfTheWingLords
+//
+//  Created by Adam Borzecki on 2013-08-21.
+//  Copyright (c) 2013 Adam Borzecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
